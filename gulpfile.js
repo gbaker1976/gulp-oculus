b51f7f1f565e4f29cf23338787fc06dfe3c18f65
Garrett Baker <garrett.baker@exacttarget.com>
2015-03-08 22:53:15 -0400

    restructuring for templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require( 'gulp' );
 var postcss = require( 'gulp-postcss' );
 var postcssNested = require( 'postcss-nested' );
-var kss = require( 'grunt-kss' );
+var kss = require( 'gulp-kss-styleguide' );
 var cssnext = require( 'gulp-cssnext' );
 var path = require( 'path' );
 var fs = require( 'fs' );
@@ -12,19 +12,27 @@ var reload = browserSync.reload;
 gulp.task(
 	'clean-dist',
 	function( cb ) {
-		wrench.rmdirSyncRecursive( './styleguides/dist' );
-		fs.mkdir( './styleguides/dist', cb );
+		wrench.rmdirSyncRecursive( 'styleguides/dist' );
+		fs.mkdir( 'styleguides/dist', cb );
 	}
 );
 
+gulp.task( 'styleguide', function(){
+	return gulp.src( 'styleguides/src/**/*.css' )
+		.pipe( kss({
+			template: 'boilerplate/templates/default.html',
+			css: 'boilerplate/grid.css'
+		}))
+		.pipe( gulp.dest( 'styleguides/dist' ) );
+});
+
 gulp.task( 'css', function(){
-	return gulp.src( './styleguides/src/css/_all.css' )
-	.pipe( cssnext({
-		compress: false
-	}))
-	.pipe( postcss( [ postcssNested ] ) )
-	.pipe( kss() )
-	.pipe( gulp.dest( './styleguides/dist/css' ) )
+	return gulp.src( 'styleguides/src/*/css/_all.css' )
+		.pipe( cssnext({
+			compress: false
+		}))
+		.pipe( postcss( [ postcssNested ] ) )
+		.pipe( gulp.dest( 'styleguides/dist' ) );
 });
 
 gulp.task( 'serve', function() {
@@ -35,3 +43,5 @@ gulp.task( 'serve', function() {
 		}
 	});
 });
+
+gulp.task( 'default', [ 'clean-dist', 'styleguide', 'css', 'serve' ] );
